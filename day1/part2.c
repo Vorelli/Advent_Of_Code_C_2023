0be5b3811d2787dfd117a9a055ebac4d754cd5c0
Natale Walter Toscano <46127615+Vorelli@users.noreply.github.com>
2023-12-02 02:55:57 -0700

    Finish day 1 part 2

--- day1/part2.c
+++ day1/part2.c
@@ -1,7 +1,26 @@
-#include "part1.h"
+#include "part2.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+
+#define FOREACH_NUMBER_WORD(NUMBER_WORD)                                       \
+  NUMBER_WORD(one)                                                             \
+  NUMBER_WORD(two)                                                             \
+  NUMBER_WORD(three)                                                           \
+  NUMBER_WORD(four)                                                            \
+  NUMBER_WORD(five)                                                            \
+  NUMBER_WORD(six)                                                             \
+  NUMBER_WORD(seven)                                                           \
+  NUMBER_WORD(eight)                                                           \
+  NUMBER_WORD(nine)
+
+#define GENERATE_ENUM(ENUM) ENUM,
+#define GENERATE_STRING(STRING) #STRING,
+
+enum NUMBER_WORDS { FOREACH_NUMBER_WORD(GENERATE_ENUM) };
+static const char *NUMBER_WORD_STRINGS[] = {
+    FOREACH_NUMBER_WORD(GENERATE_STRING)};
+
 int main() {
   solveAndPrintSolution();
   return 0;
@@ -21,21 +40,38 @@ void solveAndPrintSolution() {
   char problemArr[BUFSIZ];
   while (fgets(problemArr, sizeof problemArr, problemFile) != NULL) {
     int firstNumber = findNumber(problemArr, size, 0, true);
-    int secondNumber =
-        findNumber(problemArr, size, strlen(problemArr) - 1, false);
+    int secondNumber = findNumber(problemArr, size, strlen(problemArr), false);
     sum += (firstNumber * 10) + secondNumber;
   }
-  printf("Result %d", sum);
+  printf("Result %d\n", sum);
 }
 
 int findNumber(char file[], long size, unsigned long start, bool toTheRight) {
   char a;
   for (long i = start; toTheRight ? i < size : i >= 0;
        i += toTheRight ? 1 : -1) {
+    int stringNum = findStringNum(file, i);
+    if (stringNum != 0)
+      return stringNum;
+
     a = file[i];
     int num = atoi(&a);
-    if (num != 0 && num < 10)
+    if (num != 0)
       return num;
+  }
+  return 0;
+}
+
+int findStringNum(char file[], int i) {
+  for (int j = one; j <= nine; j++) {
+    const char *str = NUMBER_WORD_STRINGS[j];
+    for (unsigned long k = 0; k < strlen(str); k++) {
+      if (file[i + k] != str[k])
+        break;
+
+      if (k == strlen(str) - 1 && file[i + k] == str[k])
+        return j + 1;
+    }
   }
   return 0;
 }
--- day1/part2.h
+++ day1/part2.h
@@ -1,3 +1,4 @@
 #include <stdbool.h>
 void solveAndPrintSolution();
 int findNumber(char file[], long size, unsigned long i, bool toTheRight);
+int findStringNum(char file[], int i);
